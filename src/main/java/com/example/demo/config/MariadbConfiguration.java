b18f540af4dd6c3d36d5ec7c8a109f76a531636a
Jakub Krhovják <jakub.krhovjak@pricefx.eu>
2021-05-13 07:31:53 +0200

    aded maria db

---
+++ src/main/java/com/example/demo/config/MariadbConfiguration.java
@@ -0,0 +1,29 @@
+package com.example.demo.config;
+
+import org.mariadb.r2dbc.MariadbConnectionConfiguration;
+import org.mariadb.r2dbc.MariadbConnectionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+/**
+ * Created by Jakub krhovják on 5/13/21.
+ */
+@Configuration
+@Profile("maria")
+public class MariadbConfiguration {
+
+    @Bean
+    public MariadbConnectionFactory mariadbConnectionFactory() {
+        MariadbConnectionConfiguration conf = MariadbConnectionConfiguration.builder()
+                .host("localhost")
+                .port(3320)
+                .username("user")
+                .password("passport")
+                .database("reactivedb")
+                .build();
+        return new MariadbConnectionFactory(conf);
+
+    }
+
+}
