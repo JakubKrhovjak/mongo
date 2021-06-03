22ec6163afb1263095040db205339b0aa78dd055
Jakub Krhovják <jakub.krhovjak@pricefx.eu>
2021-06-03 13:51:13 +0200

    filter 2

--- fe/src/Items/Items.js
+++ fe/src/Items/Items.js
@@ -2,45 +2,40 @@ import React, {useEffect, useState} from "react";
 import {DataGrid} from "@material-ui/data-grid";
 import {httpClient} from "../httpClient";
 
-export const DEFAULT_STATE = {items: [], page: 0, pageSize: 30, sort: {}}
+export const DEFAULT_STATE = {items: [], filters: [], page: 0, pageSize: 30, sort: {}}
+
+
+const columns = [
+    {field: "id", hide: true},
+    {field: "name", headerName: "Name", width: 150},
+    {field: "description", headerName: "Description", width: 150},
+    {field: "status", headerName: "Status", width: 150},
+
+];
 
 export const Items = (props) => {
 
     const [state, setState] = useState(DEFAULT_STATE)
 
     useEffect(() => {
+        fetItems();
+    }, [state])
+
+    const fetItems = () => {
         httpClient.post("/mongo", state)
             .then(res => {
                 setState({...state, items: res.data.content})
             })
             .catch(e => console.log(e))
-    }, [])
-
-
-    const columns = [
-        {field: "id", hide: true},
-        {field: "name", headerName: "Name", width: 150},
-        {field: "description", headerName: "Description", width: 150},
-        {field: "status", headerName: "Status", width: 150},
-
-    ];
+    }
 
     const handleFilterModelChange = (event) => {
         const filters = event.filterModel.items;
-        filters.find(f => f.value) && httpClient.post("/mongo", {
-            filters: filters,
-            page: state.page,
-            pageSize: state.pageSize
-        })
-            .then(res => {
-                setState({...state, items: res.data.content})
-            })
+        filters.find(f => f.value) && setState({...state, filters})
     }
 
-    const onSortModelChange = (event, b, c) => {
-        const sort =  event.sortModel;
-        setState({...state, sort: {...event.sortModel, directions:  event.sortModel.sort}})
-        console.log(a);
+    const onSortModelChange = (event) => {
+        setState({...state, sort: {...event.sortModel, direction:  event.sortModel.sort}})
     }
 
     return (
