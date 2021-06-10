e57c1685c02121911a0329df0a392a237a2bb9fe
Jakub Krhovják <jakub.krhovjak@pricefx.eu>
2021-06-10 07:18:25 +0200

    basic alert

--- fe/src/components/Alert.js
+++ fe/src/components/Alert.js
@@ -1,15 +1,24 @@
 import React, {useEffect, useState} from "react";
 import {makeStyles} from '@material-ui/core/styles';
+import Grow from '@material-ui/core/Grow';
 import {Alert as MuiAlert, AlertTitle} from "@material-ui/lab";
+import Slide from "@material-ui/core/Slide";
 
 const useStyles = makeStyles({
     root: {
 
         zIndex: "2000 !important",
         position: "absolute",
-        opacity: "0.8",
-        width: "100%",
-    }
+        opacity: "0.8 !important",
+        width: "90%",
+        marginTop: "5%"
+    },
+    // container: {
+    //     display: "flex",
+    //     justifyContent: "center",
+    //     alignItems: "center",
+    // }
+
 });
 
 export const Alert = ({show, message, ...rest}) => {
@@ -22,10 +31,16 @@ export const Alert = ({show, message, ...rest}) => {
     }, [show])
 
     return (
-        state.show && <MuiAlert {...rest} className={classes.root} onClick={() => setState({show: false})}>
-            <AlertTitle>Error</AlertTitle>
-            {message}
-        </MuiAlert>
+        state.show &&
+
+            <Slide id="alert" in={show} timeout={1000} className={classes.root} onClick={() => setState({show: false})}>
+                <MuiAlert {...rest} className={classes.root}>
+                    <AlertTitle>Error</AlertTitle>
+                    {message}
+                </MuiAlert>
+            </Slide>
+
+
     )
 
 }
